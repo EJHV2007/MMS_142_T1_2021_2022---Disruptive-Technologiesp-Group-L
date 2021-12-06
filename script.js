43489a2a7a647333e8f716219c53659416f06d69
EJHV2007 <95361628+EJHV2007@users.noreply.github.com>
2021-12-07 00:50:45 +0800

    Initial Structure of Website
    Functional navigation, home page skeletal structure, history page skeletal structure, footer skeletal structure. Images and logo still waiting. Text content still waiting.

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('heroContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "./assets/ai_hero.json"
+})
